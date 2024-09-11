a1fbabd569c5142aab1e4c65ab72274b365d8b4f
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-09-11 19:34:54 +0200

    simplier movement

--- Project/Scripts/player_manager.gd
+++ Project/Scripts/player_manager.gd
@@ -2,154 +2,113 @@ extends CharacterBody3D
 
 # CONSTANTS
 const SENSITIVITY = 0.0024
-@export var JUMP_FORCE = 8
-const SPRINT_SPEED = 10
-const WALK_SPEED = 8
-const SPEED = 8
-const DASH_FORCE = 12
+const JUMP_FORCE = 8
+const SPRINT_SPEED = 8
+const WALK_SPEED = 6
+const GRAVITY = 9.81 * 2.5
 
-# HEAD BOOBING
-const BOB_FREQ = 2.1
-const BOB_AMP = 0.1
+# HEAD BOBBING
+const BOB_FREQ = 3.2
+const BOB_AMP = 0.08
 
 # FOV
 const BASE_FOV = 80.0
 const FOV_CHANGE = 1.6
 
-# VARIBLES
-var gravity = 9.81 * 2.5
-var bob_t = 0.0
-var steps_timer = 0.0
-var dash_remain = 1
-
+# VARIABLES
+var bob_timer = 0.0
+var speed = WALK_SPEED
+var direction = Vector3.ZERO
+var is_sprinting = false
 var temp_paused = false
-var crouching = false
-
-var jumping 
-var is_sprinting
-var direction
-var speed
 
 # ONREADY OBJECTS
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
-@onready var head_raycast = $Head/RayCast3D
 
 # MOUSE MODE
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
-	# QUIT
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	
-	# restart
 	if Input.is_action_just_pressed("r"):
 		get_tree().reload_current_scene()
 	
-	# fullscreen
 	if Input.is_action_just_pressed("m"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
-# CAMERA ROTATION
+# PROCESS USER INPUT FOR CAMERA ROTATION
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and !temp_paused:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(70))
-		
+
+# PHYSICS PROCESS (MOVEMENT & GAME LOGIC)
 func _physics_process(delta):
-	# IF NOT PAUSE 
-	if !temp_paused:
-		# GRAVITY
-		if !is_on_floor():
-			velocity.y -= gravity * delta
+	if temp_paused:
+		return
+	
+	_apply_gravity(delta)
+	_handle_movement(delta)
+	_apply_head_bobbing(delta)
+	_adjust_fov(delta)
+	_adjust_camera_tilt()
+	
+	move_and_slide()
 
-		# JUMP -------------------------------------------------------------------------------
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			if crouching:
-				velocity.y = JUMP_FORCE * 1.25
-				jumping = true
-			else:
-				velocity.y = JUMP_FORCE
-				jumping = true
-			
-		if Input.is_action_just_pressed("jump") and !is_on_floor() and dash_remain == 1:
-			velocity += Vector3(DASH_FORCE * direction.x, DASH_FORCE * direction.y, DASH_FORCE * direction.z)
-			dash_remain -= 1
-			
-		if is_on_floor():
-			dash_remain = 1
-			jumping = false
-		if !is_on_floor():
-			jumping = true
-			
-		# SLIDE/CROUCH -------------------------------------------------------------------------------
-		if Input.is_action_just_pressed("crouch"):
-			velocity = direction * SPRINT_SPEED
-			crouching = true
-		elif Input.is_action_just_released("crouch"):
-			crouching = false
-		
-		if crouching:
-			scale.y = lerp(scale.y, 0.3, 0.2)
-		else:
-			if !head_raycast.is_colliding():
-				scale.y = lerp(scale.y, 1.0, 0.2)
-		
-		# SPRINT AND WALK --------------------------------------------------------------------
-		if Input.is_action_pressed("sprint"):
-			speed = SPRINT_SPEED
-			is_sprinting = true
-		else:
-			speed = WALK_SPEED
-			is_sprinting = false
-		
-		if is_sprinting and speed >= 15:
-			speed = 15
+# APPLY GRAVITY
+func _apply_gravity(delta):
+	if !is_on_floor():
+		velocity.y -= GRAVITY * delta
+
+# HANDLE PLAYER MOVEMENT (WALK/SPRINT)
+func _handle_movement(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
 	
-		# MOVEMENT INPUTS
-		var input_dir = Input.get_vector("a", "d", "w", "s")
-		direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-		# MOVING ON FLOR/AIR AND CAMERA MOVEMENT -----------------------------------------------
-		if !crouching:
-			if is_on_floor():
-				if direction:
-					velocity.x = direction.x * speed
-					velocity.z = direction.z * speed
-				else:
-					velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-					velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-			else:
-				velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-				velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-			
-		# HEAD BOOBING ------------------------------------------
-		if !crouching:
-			bob_t += delta * velocity.length() * float(is_on_floor())
-			camera.transform.origin = _headbob(bob_t)
-		
-		# FOV ----------------------------------------------------------------
-		var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-		var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-		
-		# CAMERA ROTATION -----------------------------------------------
-		if Input.is_action_pressed("a"):
-			camera.rotation.z = lerp(camera.rotation.z, 0.045, 0.25)
-		elif Input.is_action_pressed("d"):
-			camera.rotation.z = lerp(camera.rotation.z, -0.045, 0.25)
-		else:
-			camera.rotation.z = lerp(camera.rotation.z, 0.0, 0.25)
-		
-		# MOVE
-		move_and_slide()
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+		is_sprinting = true
 	else:
-		pass
+		speed = WALK_SPEED
+		is_sprinting = false
+	
+	var input_dir = Input.get_vector("a", "d", "w", "s")
+	direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 
-# head boobing return v3 of camera pos using trigonometric equations y = sinx
+# APPLY HEAD BOBBING (BASED ON MOVEMENT)
+func _apply_head_bobbing(delta):
+	if direction.length() > 0 and is_on_floor():
+		bob_timer += delta * velocity.length()
+		camera.transform.origin = _headbob(bob_timer)
+
+# ADJUST FIELD OF VIEW BASED ON SPEED
+func _adjust_fov(delta):
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+# ADJUST CAMERA TILT (LEFT/RIGHT MOVEMENT)
+func _adjust_camera_tilt():
+	if Input.is_action_pressed("a"):
+		camera.rotation.z = lerp(camera.rotation.z, 0.045, 0.25)
+	elif Input.is_action_pressed("d"):
+		camera.rotation.z = lerp(camera.rotation.z, -0.045, 0.25)
+	else:
+		camera.rotation.z = lerp(camera.rotation.z, 0.0, 0.25)
+
+# FUNCTION TO CALCULATE HEAD BOBBING POSITION
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
--- Project/Scripts/player_sounds_manager.gd
+++ Project/Scripts/player_sounds_manager.gd
@@ -1,43 +1,26 @@
 extends Node3D
 
+@onready var player = $".."
+
 var stepsdelay = 0.3
 var stepstimer = 0.0
 
-var slidedelay = 0.75
-var slidetimer = 0.0
-
-@onready var player = $".."
-
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _process(delta):
-	
 	stepstimer += 1 * delta
-	slidetimer += 1 * delta
 	
 	if Input.is_action_just_pressed("jump"):
-		if player.dash_remain >= 1:
-			$Jump.play()
+		$Jump.play()
 	
 	if Input.is_action_pressed("w") or Input.is_action_pressed("a") or Input.is_action_pressed("s") or Input.is_action_pressed("d"):
 		if stepstimer > stepsdelay:
-			if player.is_on_floor() and !player.crouching:
+			if player.is_on_floor():
 				if player.is_sprinting:
-					$Steps.pitch_scale = 1.16
+					$Steps.pitch_scale = 1.0
 					$Steps.play()
 				else:
 					$Steps.pitch_scale = 0.92
 					$Steps.play()
 			stepstimer = 0.0
-		
-	if Input.is_action_pressed("crouch") and player.is_on_floor():
-		if slidetimer >= slidedelay:
-			if player.velocity.length() > 1:
-				$Slide.play()
-			slidetimer = 0.0
-			
-	if Input.is_action_just_released("crouch"):
-		$Slide.stop()
-	
-	
